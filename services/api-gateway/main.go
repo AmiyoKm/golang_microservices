6ea5fe9113bad20b9305a4a71d57270d1a7f9243
AmiyoKm <games.amiyo@gmail.com>
2025-07-23 16:40:53 +0600

    graceful shutdown

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
-
+	"os"
+	"os/signal"
 	"ride-sharing/shared/env"
+	"syscall"
+	"time"
 )
 
 var (
@@ -12,8 +16,6 @@ var (
 )
 
 func main() {
-	log.Println("Starting API Gateway")
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /trip/preview", handleTripPreview)
@@ -21,7 +23,30 @@ func main() {
 		Addr:    httpAddr,
 		Handler: mux,
 	}
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("HTTP server error %v", err)
+
+	serverErrors := make(chan error, 1)
+	go func() {
+
+		log.Printf("Server listening on %s", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+
+	case err := <-serverErrors:
+		log.Printf("error stating the server %v", err)
+	case sig := <-shutdown:
+		log.Printf("server is shutting down due to %s signal", sig.String())
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("could not shutdown gracefully :%v", err)
+			server.Close()
+		}
 	}
 }
